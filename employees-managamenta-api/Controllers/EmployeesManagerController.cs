a007d3f4c8d5c45b0d9038c3521c3e5376827bb5
arminCicic <armincicic@gmail.com>
2023-04-14 12:56:48 +0200

    Added Login Modal

---
+++ employees-managamenta-api/Controllers/EmployeesManagerController.cs
@@ -0,0 +1,56 @@
+﻿using employees_manager_api;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace employees_managamenta_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesManagerController : ControllerBase
+    {
+        private static List<Employee> employees = new List<Employee>
+            { new Employee {
+                Id = 1,
+                FirstName = "Armin",
+                LastName = "cicic",
+                Email = "armincicic@gmail.com",
+                Dob = "04.06.1990",
+                Gender = "male",
+                Education = "diploma",
+                Company = "readydev",
+                Experience = "two",
+
+                }};
+
+        [HttpGet]        
+        public async Task<ActionResult<List<Employee>>> Get()
+        {          
+
+            return Ok(employees);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> Get(int id)
+        {
+            var employee = employees.Find(e => e.Id == id);
+            if (employee == null)
+            
+                return BadRequest("Hero is not found");
+                return Ok(employee);
+            
+
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+
+            return Ok(employees);
+        }
+    }
+}
+
+
---
+++ employees-managamenta-api/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace employees_manager_api
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Dob { get; set; } = string.Empty;
+
+        public string Gender { get; set; } = string.Empty;
+
+        public string Education { get; set; } = string.Empty;
+
+        public string Company { get; set; } = string.Empty;
+        public string Experience { get; set; } = string.Empty;
+
+
+    }
+}
+
+
